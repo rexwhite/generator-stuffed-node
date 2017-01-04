22c3b59b956bc8a74c081830388c3ec5f86f74f5
Rex White <rexwhite@gmail.com>
2017-01-04 00:33:10 -0500

    Added author, email, website and license.

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,8 +3,14 @@ var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var camelCase = require('camel-case');
+var gitConfig = require('git-config');
 
 module.exports = Generator.extend({
+  initializing: function () {
+    this.gitc = gitConfig.sync();
+    this.gitc.user = this.gitc.user || {};
+  },
+
   prompting: function () {
     // Have Yeoman greet the user.
     this.log(yosay(
@@ -22,7 +28,25 @@ module.exports = Generator.extend({
         type: 'input',
         name: 'description',
         message: 'Project description',
-        default: 'My groovy new project!'
+        default: 'My groovy project!'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Author name',
+        default: this.gitc.user.name
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Author email address',
+        default: this.gitc.user.email
+      },
+      {
+        type: 'input',
+        name: 'website',
+        message: 'Company website (optional)',
+        store: true
       }
     ];
 
@@ -30,6 +54,13 @@ module.exports = Generator.extend({
       // To access props later use this.props.someAnswer;
       this.props = props;
       this.props.module = camelCase(props.name + 'App');
+
+      // Get license type
+      this.composeWith(require.resolve('generator-license/app'), {
+          name: props.author,
+          email: props.email,
+          website: props.website
+      });
     }.bind(this));
   },
 
